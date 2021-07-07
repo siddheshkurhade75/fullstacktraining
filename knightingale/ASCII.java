8472cddc38e792190dc5d9f6b0290d5a634f99bd
siddheshkurhade75 <86569713+siddheshkurhade75@users.noreply.github.com>
2021-07-07 19:47:14 +0530

    Add files via upload

---
+++ knightingale/ASCII.java
@@ -0,0 +1,11 @@
+package knightingale;
+
+public class ASCII {
+    public static void main(String[] args) {
+		char ch='b';
+		int ascii= ch;
+		
+		int CASTascii = (int) ch;
+		System.out.println(CASTascii);
+	}
+}
---
+++ knightingale/basic.java
@@ -0,0 +1,18 @@
+package knightingale;
+
+public class basic {
+   public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	   //data 
+	   int i=100;
+	   int j=200;
+	   
+	   //Processing of data...
+	   
+	   int sum=i+j;
+	   
+	   //output
+	   
+	   System.out.println("the result is ...:"+sum);
+}
+}
